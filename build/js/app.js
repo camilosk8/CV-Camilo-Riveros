ebfbdda74eeecc498df923aa35f99d1e26eda039
camilo r <camilosk8.rey@gmail.com>
2022-03-30 12:08:42 -0500

    Cambios en las habilidades, porcentajes

--- build/js/app.js
+++ build/js/app.js
@@ -5,7 +5,7 @@ hamburguesa.addEventListener('click', () => {
   enlaces.classList.toggle('activado');
 });
 
-document.querySelector('.icono-hamburguesa').addEventListener('click',animateBars);
+document.querySelector('.icono-hamburguesa').addEventListener('click', animateBars);
 
 let line1__bars = document.querySelector('.line1__bars-menu');
 let line2__bars = document.querySelector('.line2__bars-menu');
@@ -16,3 +16,23 @@ function animateBars() {
   line2__bars.classList.toggle('activeline2__bars-menu');
   line3__bars.classList.toggle('activeline3__bars-menu');
 }
+
+//habilidades circulos
+// let progressBar = document.querySelector(".circulo-progress")
+// let valueContainer = document.querySelector(".value-container")
+
+// let progressValue = 0;
+// let progressEndValue = 75;
+// let speed = 10;
+
+// let progress = setInterval(() => {
+//   progressValue++;
+//   valueContainer.textContent = `${progressValue}%`;
+//   progressBar.style.background = `conic-gradient(
+//     #eb282b ${progressValue * 3.6}deg,                    
+//     #000000 ${progressValue * 3.6}deg
+//   )`;
+//   if (progressValue == progressEndValue) {
+//     clearInterval(progress);
+//   }
+// }, speed);
